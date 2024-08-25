d175cf34945e556f17cbaa2bfca5f99113a1c2fb
Leonardo <leohuemaster@gmail.com>
2024-08-25 10:25:06 -0300

    fix : export add

--- lib/leo_lib.dart
+++ lib/leo_lib.dart
@@ -1,7 +1,5 @@
 library leo_lib;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/models/interfaces/ilocal_storage.dart';
+export 'src/services/local_storage.dart';
+
